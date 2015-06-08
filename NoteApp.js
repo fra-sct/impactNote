3ab9a03de1c8dffbcbe02236255adbf0c0dfa6b8
fra-sct <fra-sct@users.noreply.github.com>
2015-06-08 12:30:58 +0200

    Using a Mongo Collection instead of a js dict.

--- NoteApp.js
+++ NoteApp.js
@@ -1,10 +1,15 @@
+// NoteApp.js
+Notes = new Mongo.Collection("notes");
+
 if (Meteor.isClient) {
   Template.body.helpers({
-    notes: [
-      { text: "Note 1" },
-      { text: "Note 2" },
-      { text: "Note 3" }
-    ]
+    notes: function () {
+      // right now, I return every single note in the db,
+      // without any filtering whatsoever
+      // TODO: when adding users, filter to show only own
+      //  notes, or public notes
+      return Notes.find({});
+    }
   });
 }
 
