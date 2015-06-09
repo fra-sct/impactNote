1db369109685e6c7675bd708c5f91017929dba9b
fra-sct <fra-sct@users.noreply.github.com>
2015-06-09 12:36:11 +0200

    Added titles to notes (form, template). Not much.

--- NoteApp.js
+++ NoteApp.js
@@ -14,12 +14,15 @@ if (Meteor.isClient) {
 
   Template.note_create.events({
     "submit .new-note": function (event) {
+      var title = event.target.title.value;
       var text = event.target.text.value;
       Notes.insert({
+        title: title,
         text: text,
         creationDate: Date()
       });
       // clear the form
+      event.target.title.value = "";
       event.target.text.value = "";
       // to avoid default submitting
       return false;
