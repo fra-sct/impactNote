08b43e060e0f89a38da3e404a56c41f65b1874f1
fra-sct <fra-sct@users.noreply.github.com>
2015-06-10 14:43:07 +0200

    Moving code and templates around.

--- client/Archive.js
+++ client/Archive.js
@@ -1,3 +1,11 @@
+Template.Archive.helpers({
+  notes: function () {
+    return Notes.find({}, {
+        sort: {createdAt: -1},
+      });
+  }
+});
+
 Template.note_show_compact.events({
   "click .delete": function () {
     // TODO: ask for confirmation on note removal
